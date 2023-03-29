62f8f6942d75f6ff298d27c968e9d1b9f34bac1f
Omar-Abdullah-Shaikh-Al-Zoor <oas2713@rit.edu>
2023-03-29 10:49:13 +0400

    Game: issue where image placed improperly

--- scripts/jQuery_script.js
+++ scripts/jQuery_script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $("#InfoModal").modal('show');
-    $("#start-game").onclick(gameStart()); //add price_updated()
-    $("#restart-game").onclick();
-    $("#purchase").onclick( function () {
+    $("#start-game").click(gameStart); //add price_updated()
+    $("#restart-game").click();
+    $("#purchase").click( function () {
         console.log(price);
         if (price <= 3000) {
             console.log(price);
@@ -12,7 +12,7 @@ $(document).ready(function(){
         }
     });
 })
-const IMG_ARR =["../media/images/airpods.png", "../media/images/atari_console.png", "../media/images/broken_iphone8.png", "../media/images/broken_samsung.png", "../media/images/broken_smartphone.png", "../media/images/camera.png", "../media/images/ipad.png", "../media/images/gameboy_console.png", "../media/images/headphone.png", "../media/images/laptop.png", "../media/images/ps2_console.png", "../media/images/broken_iphone6.png", "../media/images/broken_phonemini.png", "../media/images/broken_phonemini2.png", "../media/images/broken_tv.png", "../media/images/old_tv.png", "../media/images/broken_videotape.png", "../media/images/broken_iphone.png", "../media/images/printer.png"]
+const IMG_ARR =["../media/images/atari_console.png", "../media/images/broken_iphone8.png", "../media/images/broken_samsung.png", "../media/images/broken_smartphone.png", "../media/images/camera.png", "../media/images/ipad.png", "../media/images/gameboy_console.png", "../media/images/headphone.png", "../media/images/ps2_console.png", "../media/images/broken_iphone6.png", "../media/images/broken_phonemini.png", "../media/images/broken_phonemini2.png", "../media/images/broken_tv.png", "../media/images/old_tv.png", "../media/images/broken_videotape.png", "../media/images/broken_iphone.png", "../media/images/broken_printer.png"]
 counter = 0;
 counters = []
 price = parseInt($("#price span").text());
@@ -20,34 +20,30 @@ price = parseInt($("#price span").text());
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
-function getRndInteger2(min, max) {
-    return Math.floor(Math.random() * (max - min) );
-}
-
 function gameStart() {
-    box = document.querySelector(".e-wastes div");
-    img = img_create(random);
-    
-    $(".e-wastes div").append(img);
+    eWaste_div = $(".e-wastes div")
+    img = create_img(eWaste_div);
+    eWaste_div.append(img);
     // counters.append("img"+counter);
     // counter++;
-    setTimeout(gameStart, 2000);
+    setTimeout(gameStart, 3000);
 }
-function create_img(random) {
+function create_img(eWaste_div) {
     random = getRndInteger(0,18); //random selection of images
     img = document.createElement("img");
     img.src = IMG_ARR[random];
-    img.backgroundColor = "white";
     // img.id = "drag"+counter;
     //img.setAttribute("draggable", "true");
     //img.setAttribute("ondragstart", "drag(event)");
     // img.setAttribute("id", "id=\"img"+counter+"\"");
-    img.style.top = getRndInteger2(img.height, box.offsetHeight) + "px";
-    img.style.left = getRndInteger2(img.height, box.offsetWidth)+ "px";
-    return img;
     
+    // set the image position randomly within the container
+    img.style.position = "absolute";
+    img.style.width = "10vw";
+    img.style.top = getRndInteger(0, eWaste_div.offsetHeight - img.height) + "px";
+    img.style.left = getRndInteger(0, eWaste_div.offsetWidth - img.width) + "px";
+    return img;
 }
-
 function price_updated() {
         if (price < 5000) {
             price += 50;
