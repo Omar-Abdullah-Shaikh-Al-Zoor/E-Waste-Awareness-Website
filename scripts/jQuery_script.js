def764f45c569ab539580f3977d03ca52eeb9337
Omar-Abdullah-Shaikh-Al-Zoor <oas2713@rit.edu>
2023-03-30 15:39:15 +0400

    Game: Random positions for images fixed

--- scripts/jQuery_script.js
+++ scripts/jQuery_script.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         }
     });
 });
-const IMG_ARR =["../media/images/atari_console.png", "../media/images/broken_iphone8.png", "../media/images/broken_samsung.png", "../media/images/broken_smartphone.png", "../media/images/camera.png", "../media/images/ipad.png", "../media/images/gameboy_console.png", "../media/images/headphone.png", "../media/images/ps2_console.png", "../media/images/broken_iphone6.png", "../media/images/broken_phonemini.png", "../media/images/broken_phonemini2.png", "../media/images/broken_tv.png", "../media/images/old_tv.png", "../media/images/broken_videotape.png", "../media/images/broken_iphone.png", "../media/images/broken_printer.png"]
+const IMG_ARR =["../media/images/atari_console.png", "../media/images/broken_iphone8.png", "../media/images/broken_samsung.png", "../media/images/broken_smartphone.png", "../media/images/camera.png", "../media/images/gameboy_console.png", "../media/images/headphone.png", "../media/images/ps2_console.png", "../media/images/broken_iphone6.png", "../media/images/broken_phonemini.png", "../media/images/broken_phonemini2.png", "../media/images/broken_tv.png", "../media/images/old_tv.png", "../media/images/broken_videotape.png", "../media/images/broken_iphone.png", "../media/images/broken_printer.png"]
 counter = 0;
 counters = []
 price = parseInt($("#price span").text());
@@ -21,85 +21,49 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
     }
 
-    function getRndPosition(container) {
-        containerRect = container.getBoundingClientRect();
-        containerWidth = containerRect.width;
-        containerHeight = containerRect.height;
-      
-        imgWidth = 10; // width of the image in vw
-        imgHeight = (imgWidth * containerHeight) / 100; // height of the image in pixels
-      
-        // get the container's absolute position on the page
-        containerTop = containerRect.top + window.scrollY;
-        containerLeft = containerRect.left + window.scrollX;
-      
-        // keep trying random positions until one is found that doesn't overlap with existing images
-        while (true) {
-          top = getRndInteger(0, containerHeight - imgHeight);
-          left = getRndInteger(0, containerWidth - imgWidth);
-      
-          // check if the new image overlaps with any existing images
-          overlaps = false;
-          for (i = 0; i < eWaste_div.children.length; i++) {
-            otherImg = eWaste_div.children[i];
-            if (otherImg == img) {
-              continue; // skip comparison with self
-            }
-            otherImgRect = otherImg.getBoundingClientRect();
-            otherImgLeft = otherImgRect.left - containerLeft;
-            otherImgTop = otherImgRect.top - containerTop;
-            if (
-              left < otherImgLeft + otherImgRect.width &&
-              left + imgWidth > otherImgLeft &&
-              top < otherImgTop + otherImgRect.height &&
-              top + imgHeight > otherImgTop
-            ) {
-              overlaps = true;
-              break;
-            }
-          }
-          if (!overlaps) {
-            break;
-          }
-        }
-      
-        return { top: top + containerTop, left: left + containerLeft };
-      }
-      
-
-
-function gameStart() {
-    eWaste_div = document.querySelector(".e-wastes");
-    img = create_img(eWaste_div);
-    eWaste_div.append(img);
-    console.log("gameStart");
-    // counters.append("img"+counter);
-    // counter++;
-    setTimeout(gameStart, 1000);
-}
-
-function create_img(eWaste_div) {
-    console.log("gameStart2");
-    random = getRndInteger(0, IMG_ARR.length-1);
-    img = document.createElement("img");
-    img.src = IMG_ARR[random];
-    img.style.position = "absolute";
-    img.style.width = "10vw";
-     // img.id = "drag"+counter;
-    //img.setAttribute("draggable", "true");
-    //img.setAttribute("ondragstart", "drag(event)");
-    // img.setAttribute("id", "id=\"img"+counter+"\"");
-    
-    // set the image position randomly within the container
-    console.log("gameStart3");
-
-    var pos = getRndPosition(eWaste_div);
-    console.log("gameStart7");
-
-    img.style.top = pos.top + "px";
-    img.style.left = pos.left + "px";
-    return img;
+    function gameStart() {
+        console.log("gameStart");
+        const eWaste_div = document.querySelector(".e-wastes");
+        const img = create_img(eWaste_div);
+        console.log("gameStart10");
+        eWaste_div.append(img);
+        console.log("gameStart11");
+        //generatedImagesArray.push(img.id);  // push only the id of the image
+        console.log("gameStart12");
+        setTimeout(gameStart, 1000);
     }
+    
+    function create_img(eWaste_div) {
+        console.log("gameStart2");
+        const random = getRndInteger(0, IMG_ARR.length-1);
+        const img = document.createElement("img");
+        //const id = "img" + counter;
+       // img.id = id;
+        //counter++;
+        img.src = IMG_ARR[random];
+        img.style.position = "absolute";
+        img.style.width = "10vw";
+        //img.draggable = true;
+        //counters.push(id);
+        console.log("gameStart3");
+        const pos = getRndPosition(eWaste_div);
+        console.log("gameStart7");
+        img.style.left = (pos.left / window.innerWidth) * 100 + "vw"; // convert pixel left position to viewport units
+        console.log("gameStart8");
+        img.style.top = pos.top + "px";
+        console.log("gameStart9");
+        return img;
+    }
+    
+    function getRndPosition(eWaste_div) {
+        console.log("gameStart4");
+        const rect = eWaste_div.getBoundingClientRect();
+        console.log("gameStart5");
+        const top = getRndInteger(rect.top, rect.bottom - 50);
+        const left = getRndInteger(0, 750);
+        console.log("gameStart6");
+        return { top, left };
+    }   
 
 function price_updated() {
         if (price < 5000) {
