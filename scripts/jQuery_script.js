fd6823eb1fd0d503ca40d8a9bf5bebe3b7c4019e
Mohammad <mohammad.alabed132@gmail.com>
2023-03-31 15:18:55 +0400

    Fixed Image Files Path

--- scripts/jQuery_script.js
+++ scripts/jQuery_script.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         }
     });
 });
-const IMG_ARR =["../media/images/atari_console.png", "../media/images/broken_iphone8.png", "../media/images/broken_samsung.png", "../media/images/broken_smartphone.png", "../media/images/camera.png", "../media/images/gameboy_console.png", "../media/images/headphone.png", "../media/images/ps2_console.png", "../media/images/broken_iphone6.png", "../media/images/broken_phonemini.png", "../media/images/broken_phonemini2.png", "../media/images/broken_tv.png", "../media/images/old_tv.png", "../media/images/broken_videotape.png", "../media/images/broken_iphone.png", "../media/images/broken_printer.png"]
+const IMG_ARR =["../E-Waste-Awareness-Website/media/images/atari_console.png", "../E-Waste-Awareness-Website/media/images/broken_iphone8.png", "../E-Waste-Awareness-Website/media/images/broken_samsung.png", "../E-Waste-Awareness-Website/media/images/broken_smartphone.png", "../E-Waste-Awareness-Website/media/images/camera.png", "../E-Waste-Awareness-Website/media/images/gameboy_console.png", "../E-Waste-Awareness-Website/media/images/headphone.png", "../E-Waste-Awareness-Website/media/images/ps2_console.png", "../E-Waste-Awareness-Website/media/images/broken_iphone6.png", "../E-Waste-Awareness-Website/media/images/broken_phonemini.png", "../E-Waste-Awareness-Website/media/images/broken_phonemini2.png", "../E-Waste-Awareness-Website/media/images/broken_tv.png", "../E-Waste-Awareness-Website/media/images/old_tv.png", "../E-Waste-Awareness-Website/media/images/broken_videotape.png", "../E-Waste-Awareness-Website/media/images/broken_iphone.png", "../E-Waste-Awareness-Website/media/images/broken_printer.png"]
 counter = 0;
 counters = []
 price = parseInt($("#price span").text());
@@ -21,66 +21,64 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
     }
 
-    function gameStart() {
-        console.log("gameStart");
-        const eWaste_div = document.querySelector(".e-wastes");
-        const img = create_img(eWaste_div);
-        console.log("gameStart10");
-        eWaste_div.append(img);
-        console.log("gameStart11");
-        //generatedImagesArray.push(img.id);  // push only the id of the image
-        console.log("gameStart12");
-        setTimeout(gameStart, 1000);
-    }
+function gameStart() {
+    console.log("gameStart");
+    const eWaste_div = document.querySelector(".e-wastes");
+    const img = create_img(eWaste_div);
+    console.log("gameStart10");
+    eWaste_div.append(img);
+    console.log("gameStart11");
+    //generatedImagesArray.push(img.id);  // push only the id of the image
+    console.log("gameStart12");
+    setTimeout(gameStart, 1000);
+}
     
-    function create_img(eWaste_div) {
-        console.log("gameStart2");
-        const random = getRndInteger(0, IMG_ARR.length-1);
-        const img = document.createElement("img");
-        const id = "img" + counter;
-        img.id = id;
-        counter++;
-        img.src = IMG_ARR[random];
-        img.style.position = "absolute";
-        img.style.width = "10vw";
-        img.draggable = true;
-        counters.push(id);
-        console.log("gameStart3");
-        const pos = getRndPosition(eWaste_div);
-        console.log("gameStart7");
-        img.style.left = (pos.left / window.innerWidth) * 100 + "vw"; // convert pixel left position to viewport units
-        console.log("gameStart8");
-        img.style.top = pos.top + "px";
-        console.log("gameStart9");
-        return img;
-    }
+function create_img(eWaste_div) {
+    console.log("gameStart2");
+    const random = getRndInteger(0, IMG_ARR.length-1);
+    const img = document.createElement("img");
+    const id = "img" + counter;
+    img.id = id;
+    counter++;
+    img.src = IMG_ARR[random];
+    img.style.position = "absolute";
+    img.style.width = "10vw";
+    img.draggable = true;
+    counters.push(id);
+    console.log("gameStart3");
+    const pos = getRndPosition(eWaste_div);
+    console.log("gameStart7");
+    img.style.left = (pos.left / window.innerWidth) * 100 + "vw"; // convert pixel left position to viewport units
+    console.log("gameStart8");
+    img.style.top = pos.top + "px";
+    console.log("gameStart9");
+    return img;
+}
     
-    function getRndPosition(eWaste_div) {
-        console.log("gameStart4");
-        const rect = eWaste_div.getBoundingClientRect();
-        console.log("gameStart5");
-        const top = getRndInteger(rect.top, rect.bottom - 50);
-        const left = getRndInteger(0, 750);
-        console.log("gameStart6");
-        return { top, left };
-    }   
+function getRndPosition(eWaste_div) {
+    console.log("gameStart4");
+    const rect = eWaste_div.getBoundingClientRect();
+    console.log("gameStart5");
+    const top = getRndInteger(rect.top, rect.bottom - 50);
+    const left = getRndInteger(0, 750);
+    console.log("gameStart6");
+    return { top, left };
+}   
 
 function price_updated() {
-        if (price < 5000) {
-            price += 50;
-            $("#price span").text(price);
-            setTimeout(price_updated, 3000);
-        }
-        else if (price >= 5000){
-            $("#Win-lose-Modal p").text("Good luck next time");
-            $("#Win-lose-Modal p").css({"color":"red"});
-            $("#Win-lose-Modal").modal('show');
-        }
-        
+    if (price < 5000) {
+        price += 50;
+        $("#price span").text(price);
+        setTimeout(price_updated, 3000);
+    }
+    else if (price >= 5000){
+        $("#Win-lose-Modal p").text("Good luck next time");
+        $("#Win-lose-Modal p").css({"color":"red"});
+        $("#Win-lose-Modal").modal('show');
     }
     
-
-
+}
+    
 
 function allowDrop(ev) {
     ev.preventDefault();   //to change default settings from preventing dragging to allow it
